0490820995debddf1fdefa2dd7f06b0ea3ceade6
qiaoshipeng <qiaoshipeng>
2018-04-14 12:22:06 +0800

    123

--- src/test/java/com/xuanli/oepcms/other/SyncPic.java
+++ src/test/java/com/xuanli/oepcms/other/SyncPic.java
@@ -16,10 +16,10 @@ import com.xuanli.oepcms.util.ThirdAliOSSUtil;
  * @author  QiaoYu[www.codelion.cn]
  */
 public class SyncPic extends BaseTest{
-	//@Autowired
+//	@Autowired
 //	ThirdAliOSSUtil thirdAliOSSUtil;
-	
-	//@Test
+//	
+//	@Test
 	public void uploadPic() {
 //		thirdAliOSSUtil.converterFileTest("1522047667866_e4638c544b87410f943ea5d5a20d0f1d.jpg");
 //		thirdAliOSSUtil.converterFileTest("1522056647864_dc09c0bab105485c827aecda0d22d540.jpg");
@@ -32,5 +32,14 @@ public class SyncPic extends BaseTest{
 //		thirdAliOSSUtil.converterFileTest("1522057343093_dfdc6e19119340259fd1cc4fb6dcf21f.jpg");
 //		thirdAliOSSUtil.converterFileTest("1522057382685_a192eddef6e04bce9f05740f5528f75d.jpg");
 //		thirdAliOSSUtil.converterFileTest("1522057462836_798a8c63c80544e1ab739c8502a93366.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522293588137_6ed56671097647f9a8f5cff783a90db4.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522293677849_0ec0352f61de4cb093a2611cc96b38de.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522293716321_9f23a016e5ce40a9aab50d50a8ed6f47.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522295116324_d6c34fb5f07c476da3db3d08c9242d67.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522296571939_b39c71a7d895403482c47d649630f598.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522296726830_0f31afc3f0b3486db3da196107e17fe8.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522296781015_a9fe99961b6f4bf6b9571cecb29f4a27.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522296825309_fa5ee630e11f4b89925333bdf644e6ed.jpg");
+//		thirdAliOSSUtil.converterFileTest("1522297094641_f5fab477aafa458fa981e549b30446b5.jpg");
 	}
 }
